5d026bdbe3043450d2635cd44ded98d52d0d0bc3
Jean Uchoa <jean.uchoa@gmail.com>
2023-11-29 11:30:22 -0300

    move curl stuff to a function

--- getcurl.c
+++ getcurl.c
@@ -13,33 +13,31 @@ char* build_shodan_url(char* search_query) {
 
   return url;
 }
+
+void fetch_shodan_data(char* url) {
+  CURL *curl;
  
+  curl = curl_easy_init();
+
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    res = curl_easy_perform(curl);
+
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl error: %s\n", curl_easy_strerror(res));
+
+    curl_easy_cleanup(curl);
+  }
+}
+
 /* compiling: gcc getcurl.c -o getcurl -lcurl */
 int main(int argc, char *argv[])
 {
   char* search_query = "android%20debug%20bridge%20product:%22Android%20Debug%20Bridge%22";
-  char* final_size = build_shodan_url(search_query);
-  printf("FINAL URL: %s\n", final_size);
+  char* url = build_shodan_url(search_query);
+  printf("FINAL URL: %s\n", url);
 
   return 0;
-  /* CURL *curl; */
-  /* CURLcode res; */
- 
-  /* curl = curl_easy_init(); */
-
-  /* if(curl) { */
-    /* "https://api.shodan.io/shodan/host/search?key=$api&query=android debug bridge" */
-    /* wget "https://api.shodan.io/shodan/host/search?key=$api&query=android%20debug%20bridge%20product:%22Android%20Debug%20Bridge%22&facets={facets}&page=$i" */
-
-    /* curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com"); */
-    /* curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); */
-
-    /* res = curl_easy_perform(curl); */
-
-    /* if(res != CURLE_OK) */
-      /* fprintf(stderr, "curl error: %s\n", curl_easy_strerror(res)); */
-
-    /* curl_easy_cleanup(curl); */
-  /* } */
-
 }
