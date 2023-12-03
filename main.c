e8071d4af4eebc67315a9753afd4c09347898cdd
Jean Uchoa <jean.uchoa@gmail.com>
2023-12-03 00:39:10 -0300

    generate data filename

--- main.c
+++ main.c
@@ -6,8 +6,7 @@
 char* build_shodan_url(char* search_query) {
   const char* base_url = "https://api.shodan.io/shodan/host/search?key=%s&query=%s";
   const char* shodan_api_key = getenv("SHODAN_API_KEY");
-  const int total = strlen(base_url) + strlen(shodan_api_key) + strlen(search_query);
-  char* url = (char*) malloc(total * sizeof(char));
+  char* url = (char*) malloc(strlen(base_url) + strlen(shodan_api_key) + strlen(search_query));
 
   sprintf(url, base_url, shodan_api_key, search_query);
 
@@ -28,16 +27,29 @@ void fetch_shodan_data(char* url) {
 
     if(res != CURLE_OK)
       fprintf(stderr, "curl error: %s\n", curl_easy_strerror(res));
-
     curl_easy_cleanup(curl);
   }
+}
+
+char* shodan_data_filename() {
+  srand(time(NULL));
+
+  const char* filename_template = "shodandata-%d.json";
+  const int unique_id_int = rand();
+  const int unique_id_length = snprintf(NULL, 0, "%d", unique_id_int);
+  char* filename = (char *) malloc(strlen(filename_template) + unique_id_length);
+
+  sprintf(filename, filename_template, unique_id_int);
+
+  return filename;
 }
 
 int main()
 {
   char* search_query = "android%20debug%20bridge%20product:%22Android%20Debug%20Bridge%22";
-  char* url = build_shodan_url(search_query);
-  printf("FINAL URL: %s\n", url);
+  printf("shodan url: %s\n", build_shodan_url(search_query));
+
+  printf("filename: %s\n", shodan_data_filename());
 
   return 0;
 }
