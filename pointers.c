b6934acd7c05bc07d47594ac4e41c9493fecb3ec
Jean Uchoa <jean.uchoa@gmail.com>
2025-02-05 19:40:39 -0300

    add reverse string method

--- pointers.c
+++ pointers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void swap(int *a, int *b) {
   int temp = *a;
@@ -48,10 +49,56 @@ void basic() {
   printf("pointer value after in main: %d\n", a);
 }
 
+void reverse_string(char *string) {
+  const size_t string_size = strlen(string);
+  // + 1 for null terminator
+  char *reversed_string = malloc(string_size + 1);
+  if (reversed_string == NULL) {
+    fprintf(stderr, "error allocating memory\n");
+    return;
+  }
+
+  for (size_t i = 0; i < string_size; i++) {
+    reversed_string[i] = string[string_size - i - 1];
+  }
+  reversed_string[string_size] = '\0';
+
+  printf("string: %s\n", string);
+  printf("reversed: %s\n", reversed_string);
+
+  return;
+}
+
+void dynamic_array(int size) {
+  int *array = malloc(size * sizeof(int));
+  if (array == NULL) {
+    fprintf(stderr, "error allocating memory\n");
+    return;
+  }
+
+  for (int i = 0; i < size; i++) {
+    array[i] = i * 2;
+  }
+
+  int *p = array;
+  for (int i = 0; i < size; i++) {
+    printf("%d\n", *p);
+    p++;
+  }
+
+  // free memory
+  free(array);
+  // avoid dangling arrays
+  array = NULL;
+}
 int main(void) {
-  int numbers[] = {1,2,3,4,5};
-  int number_size = sizeof(numbers) / sizeof(numbers[0]);
-  printf("size of array: %d\n", number_size);
+  /* int numbers[] = {1,2,3,4,5}; */
+  /* int number_size = sizeof(numbers) / sizeof(numbers[0]); */
+  /* printf("size of array: %d\n", number_size); */
   /* sum_array(numbers, number_size); */
-  print_pointer(&numbers[0]);
+
+  /* char *string = "hello world"; */
+  /* reverse_string(string); */
+
+  dynamic_array(5);
 }
